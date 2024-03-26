29c657985f69e3e08449885aa077d9c5093d64bc
kent riggs <k2r3@proton.me>
2024-03-26 12:14:38 -0700

    adjusted for feedback removing instantiation and added a delete class

--- server/app.py
+++ server/app.py
@@ -3,9 +3,6 @@ from config import app, db, api
 from models import Project
 from flask_restful import Resource, Api
 
-app = Flask(__name__)
-api = Api(app)
-
 class Projects(Resource):
     def get(self):
 
@@ -13,18 +10,20 @@ class Projects(Resource):
         projects_data = [project.to_dict() for project in projects]
         return make_response(jsonify(projects_data), 200)
     
+class ProjectDel(Resource):
+    
     def delete(self, project_id):
-        projects = Project.query.get(project_id)
-        if projects is None:
+        projectdel = Project.query.get(project_id)
+        if projectdel is None:
             return make_response(jsonify(error="Project was not found"), 404)
         
-        db.session.delete(projects)
+        db.session.delete(projectdel)
         db.session.commit()
 
         return make_response('', 204)
 
 api.add_resource(Project, '/projects')
-api.add_resource(Project, '/projects/<int:project_id')
+api.add_resource(Project, '/projectdel/<int:project_id')
 
 if __name__ == '__main__':
     app.run()
