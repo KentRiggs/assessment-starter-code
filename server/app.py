7f216726c64134f046546a94c3fe5148f05e8c07
kent riggs <k2r3@proton.me>
2024-03-26 12:09:54 -0700

    added get and delete methods

--- server/app.py
+++ server/app.py
@@ -1,7 +1,30 @@
-from flask import request, make_response
+from flask import Flask, make_response, jsonify
 from config import app, db, api
 from models import Project
-from flask_restful import Resource
+from flask_restful import Resource, Api
 
-import ipdb
+app = Flask(__name__)
+api = Api(app)
 
+class Projects(Resource):
+    def get(self):
+
+        projects = Project.query.all()
+        projects_data = [project.to_dict() for project in projects]
+        return make_response(jsonify(projects_data), 200)
+    
+    def delete(self, project_id):
+        projects = Project.query.get(project_id)
+        if projects is None:
+            return make_response(jsonify(error="Project was not found"), 404)
+        
+        db.session.delete(projects)
+        db.session.commit()
+
+        return make_response('', 204)
+
+api.add_resource(Project, '/projects')
+api.add_resource(Project, '/projects/<int:project_id')
+
+if __name__ == '__main__':
+    app.run()
